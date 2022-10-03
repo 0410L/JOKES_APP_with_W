e526e8bd525e15f4b627c093ef38218a51f41a8a
0410L <oriol.busquet@gmail.com>
2022-10-03 10:57:46 +0200

    1.2

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 //escribir variable en typescript
-var texto = "demo";
-var numero = 10001;
-//las constantes no pueden variar su contenido nunca.d
+var text = "texto";
+var number = 1000;
+var verdadero = true;
+var cualquier = "poner lo que sea";
+verdadero = false;
+//las constantes no pueden variar su contenido nunca.
+var nombre = "Oriol Busquet";
+console.log(text, number, verdadero);
+//arrays
+var personas = ["Victor", "Paco", "Pepe", "Juan"];
+var div_personas = document.querySelector("#personas");
+div_personas.innerHTML = "<ul>";
+personas.map(function (persona) {
+    div_personas.innerHTML += "<li>" + persona + "</li>";
+});
+div_personas.innerHTML += "</ul>";
--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 //escribir variable en typescript
-let texto: string = "demo";
-let numero: number = 10001;
+let text: string = "texto";
+let number: number = 1000;
+let verdadero: boolean = true;
+let cualquier: any = "poner lo que sea"
+
+verdadero = false;
 
 
+//las constantes no pueden variar su contenido nunca.
+const nombre: string = "Oriol Busquet"
+
+console.log (text, number, verdadero);
+
+//arrays
+let personas: string[] = ["Victor", "Paco", "Pepe", "Juan"];
+
+let div_personas:HTMLElement | null = document.querySelector("#personas");
+
+div_personas.innerHTML = "<ul>";
+
+personas.map((persona) => {
+    div_personas.innerHTML += "<li>"+persona+"</li>";
+})
+
+div_personas.innerHTML += "</ul>";
 
 
-//las constantes no pueden variar su contenido nunca.d
