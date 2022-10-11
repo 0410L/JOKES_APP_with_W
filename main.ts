1e59b691d0fb8f2bb15966e1c4818d334b6743c5
0410L <oriol.busquet@gmail.com>
2022-10-11 02:34:14 +0200

    6.4

--- main.ts
+++ main.ts
@@ -77,7 +77,7 @@ function votacion(){
 function fondos () {
     //const image = 14;
     let shapes = ['blob-1.svg','blob-2.svg','blob-3.svg','blob-4.svg','blob-5.svg','blob-6.svg','blob-7.svg','blob-8.svg','blob-9.svg','blob-10.svg','blob-11.svg','blob-12.svg','blob-13.svg','blob-14.svg']
-    let randomShapes = shapes[Math.floor(Math.random()*shapes.length)]
+    let randomShapes = shapes[Math.floor(Math.random()*shapes.length)];
     document.body.style.backgroundImage=("url(images/"+(randomShapes)+")");
 }
 
